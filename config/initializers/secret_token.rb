c4485a458299f403d6687c54b1b0b806d7ac35e8
Ngoc <ngocnvtech@gmail.com>
2013-03-01 15:11:04 +0700

    Add a commit controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '48f2f514d03a0c77be248bd01e8c00201fbb2e90da0bb27b1518f7f54534af4850041c6991210c5caae01c9d89727fcce6048ab698c24c820896ac3fc6f3fdd4'
